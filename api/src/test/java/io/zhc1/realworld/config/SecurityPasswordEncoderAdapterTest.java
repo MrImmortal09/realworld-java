1317f16a920c0ef9a013e06d277e584476a2c84c
MrImmortal09 <MrImmortal09@users.noreply.github.com>
2025-01-01 00:00:00 +0530

    Consolidated initial commit up to 2024

---
+++ api/src/test/java/io/zhc1/realworld/config/SecurityPasswordEncoderAdapterTest.java
@@ -0,0 +1,43 @@
+package io.zhc1.realworld.config;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.zhc1.realworld.core.model.PasswordEncoder;
+
+class SecurityPasswordEncoderAdapterTest {
+    PasswordEncoder sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new SecurityPasswordEncoderAdapter();
+    }
+
+    @Test
+    void testMatchesMethodWithCorrectCredentials() {
+        // given
+        String rawPassword = "TestPassword";
+        String encodedPassword = sut.encode(rawPassword);
+
+        // when
+        boolean result = sut.matches(rawPassword, encodedPassword);
+
+        // then
+        assertTrue(result);
+    }
+
+    @Test
+    void testMatchesMethodWithIncorrectCredentials() {
+        // given
+        String encodedPassword = sut.encode("CorrectPassword");
+
+        // when
+        boolean result = sut.matches("WrongPassword", encodedPassword);
+
+        // then
+        assertFalse(result);
+    }
+}
